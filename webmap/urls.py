c6a42769d1a9d7307a5d2fc456cbefef06dfdeab
Mathieu Leplatre <mathieu.leplatre@makina-corpus.com>
2013-09-04 10:18:53 +0200

    Register view in url patterns

--- webmap/urls.py
+++ webmap/urls.py
@@ -2,11 +2,14 @@ from django.views.generic import TemplateView
 from django.conf.urls import patterns, url
 
 from djgeojson.views import GeoJSONLayerView
+from geojson_tiles.views import GeoJSONTile
 
 from .models import WeatherStation
 
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home'),
-    url(r'^data.geojson$', GeoJSONLayerView.as_view(model=WeatherStation), name='data')
+    url(r'^data.geojson$', GeoJSONLayerView.as_view(model=WeatherStation), name='data'),
+    url(r'^data/(?P<z>\d+)/(?P<x>\d+)/(?P<y>\d+).json$',
+        GeoJSONTile(WeatherStation, geometry_field='geom', trim_to_boundary=True), name='tiled_data')
 )
